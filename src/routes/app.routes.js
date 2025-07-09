2b185b5a28e5781718dfc7b304686d15d6289f52
Erik Larson <eriklarsonco@gmail.com>
2025-07-09 10:42:51 -0400

    updated app router to send static html

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,10 +1,11 @@
 import express from 'express'
+import path from 'path'
 
 export const appRoutes = express.Router()
 
 // render homepage
 appRoutes.get('/', (req, res) => {
-  res.status(200).send('Hello, world!')
+  res.status(200).sendFile(path.join(__dirname, '..', '..', 'public', 'index.html'))
 })
 
 // redirect to homepage
